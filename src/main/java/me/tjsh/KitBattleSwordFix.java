bd37090e1c055a75c57f7ea7a90abb31d995fed7
tjshtqwq <128292902+tjshtqwq@users.noreply.github.com>
2025-02-04 18:16:06 +0800

    Add files via upload

--- src/main/java/me/tjsh/KitBattleSwordFix.java
+++ src/main/java/me/tjsh/KitBattleSwordFix.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -19,7 +20,7 @@ public final class KitBattleSwordFix extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        // getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(this, this);
         BukkitRunnable a = new BukkitRunnable() {
             @Override
             public void run() {
@@ -55,6 +56,10 @@ public final class KitBattleSwordFix extends JavaPlugin implements Listener {
             }
         };
         a.runTaskTimer(this, 0, 1);
+    }
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent e) {
+        e.setCancelled(true);
     }
 
     @Override
